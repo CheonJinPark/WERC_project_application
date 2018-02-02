dad66a4eb80cea606ad1456a69085b9816d20af8
Cheonjin Park <cheon,park@uconn.edu>
2018-02-02 17:03:43 -0500

    test2

---
+++ app/src/main/java/uconn/werc_project_application/NetworkTask.java
@@ -0,0 +1,41 @@
+package uconn.werc_project_application;
+
+import android.content.ContentValues;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+/**
+ * Created by pcj04 on 2018-01-30.
+ */
+public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+    private String url;
+    private ContentValues values;
+
+    public NetworkTask(String url, ContentValues values) {
+
+        this.url = url;
+        this.values = values;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        String result; // 요청 결과를 저장할 변수.
+        RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+        result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+        return result;
+    }
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+       // tv_outPut.setText(s);
+    }
+
+
+
+}
