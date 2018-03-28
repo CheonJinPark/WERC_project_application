809bad2af5f754c9f90f124055540d5c5cc28a88
Bill <williamdbrown96@gmail.com>
2018-03-27 20:39:50 -0400

    WIP: Linking the BLE to the Cloud      Added background timer task for scheduled uploads.      Added scheduled write to BLE device.
         TODO:
         Read operations after sequential uploads.
         - Bill

--- app/src/main/java/uconn/werc_project_application/ble/BLEDataLinker.java
+++ app/src/main/java/uconn/werc_project_application/ble/BLEDataLinker.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import uconn.werc_project_application.R;
 
@@ -27,17 +28,13 @@ import static android.content.ContentValues.TAG;
 
 public class BLEDataLinker {
 
-    private static BluetoothGattCharacteristic mWriteCharacteristic;
-    private static BluetoothGattCharacteristic mNotifyCharacteristic;
+    private BluetoothGattCharacteristic mWriteCharacteristic;
+    private BluetoothGattCharacteristic mNotifyCharacteristic;
     private static String mData;
     private static View mView;
     private static TextView mTv_char;
     private static TextView mTv_rx;
     private static BLEDataLinker instance = null;
-    private static Service_BLE_GATT service;
-
-    private ListAdapter_BLE_Services expandableListAdapter;
-    private ExpandableListView expandableListView;
 
     private ArrayList<BluetoothGattService> services_ArrayList;
     private HashMap<String, BluetoothGattCharacteristic> characteristics_HashMap;
@@ -101,6 +98,11 @@ public class BLEDataLinker {
     private BLEDataLinker(Context context) {
         this.address = address;
         this.context = context;
+
+        services_ArrayList = new ArrayList<BluetoothGattService>();
+        characteristics_HashMap = new HashMap<String, BluetoothGattCharacteristic>();
+        characteristics_HashMapList = new HashMap<String, ArrayList<BluetoothGattCharacteristic>>();
+
     }
 
     public void connectBLE() {
@@ -109,6 +111,7 @@ public class BLEDataLinker {
             mBTLE_Service_Intent = new Intent(context, Service_BLE_GATT.class);
             context.bindService(mBTLE_Service_Intent, mBTLE_ServiceConnection, Context.BIND_AUTO_CREATE);
             context.startService(mBTLE_Service_Intent);
+            determineCharacteristics();
             Log.d("BLEDataLinker", "Linker Declared.");
         }
     }
@@ -135,22 +138,62 @@ public class BLEDataLinker {
         else
             this.mData = "--";
     }
+    public void determineCharacteristics()
+    {
+        if (mBTLE_Service != null) {
 
+            services_ArrayList.clear();
+            characteristics_HashMap.clear();
+            characteristics_HashMapList.clear();
+
+            List<BluetoothGattService> servicesList = mBTLE_Service.getSupportedGattServices();
+
+            for (BluetoothGattService service : servicesList) {
+
+                services_ArrayList.add(service);
+
+                List<BluetoothGattCharacteristic> characteristicsList = service.getCharacteristics();
+                ArrayList<BluetoothGattCharacteristic> newCharacteristicsList = new ArrayList<>();
+
+                for (BluetoothGattCharacteristic characteristic: characteristicsList) {
+                    characteristics_HashMap.put(characteristic.getUuid().toString(), characteristic);
+                    int properties = characteristic.getProperties();
+
+                    if (BLEUtilities.hasWriteProperty(properties) != 0)
+                    {
+                        mWriteCharacteristic = characteristic;
+                        Log.d("BLEDataLinker", "Found Write characteristic");
+                        Log.d("BLEDataLinker", mWriteCharacteristic.getUuid().toString());
+                    } else if (BLEUtilities.hasNotifyProperty(properties) != 0)
+                    {
+                        mNotifyCharacteristic = characteristic;
+                        Log.d("BLEDataLinker", "Found Notify characteristic");
+                    }
+                    newCharacteristicsList.add(characteristic);
+                }
+
+                characteristics_HashMapList.put(service.getUuid().toString(), newCharacteristicsList);
+            }
+        }
+    }
     public int writeToBLE(String msg)
     {
-        if (mWriteCharacteristic != null && service != null) {
+        if (mWriteCharacteristic != null && mBTLE_Service != null) {
                 mWriteCharacteristic.setValue(msg);
-                service.writeCharacteristic(mWriteCharacteristic);
+                mBTLE_Service.writeCharacteristic(mWriteCharacteristic);
+                Log.d("BLEDataLinker", "How is this happening.");
                 return 1;
-            }
-        else
+        }
+        else {
+            determineCharacteristics();
             return 0;
+        }
 
     }
 
     public void setService(Service_BLE_GATT service)
     {
-        this.service = service;
+        this.mBTLE_Service = service;
     }
     public Context getContext() { return context; }
     public void setAddress(String address) { this.address = address; }
--- app/src/main/java/uconn/werc_project_application/data/SendDataService.java
+++ app/src/main/java/uconn/werc_project_application/data/SendDataService.java
@@ -71,12 +71,9 @@ public class SendDataService extends Service {
                     // My function happens in here.
                     DummyDataGenerator.initialize(SensorContentContract.Sensordata.PROJECTION_ALL);
                     ContentValues values = DummyDataGenerator.getInstance().generate(0.0, 3.0);
-                    if (BLEDataLinker.getInstance() != null)
+                    if (BLEDataLinker.getInstance() != null) {
                         Toast.makeText(getApplicationContext(), Integer.toString(BLEDataLinker.getInstance().writeToBLE("test")), Toast.LENGTH_LONG).show();
-
-
-
-
+                    }
 
                     AsyncQueryHandler queryHandler = new AsyncQueryHandler(contentResolver) {
                         @Override
